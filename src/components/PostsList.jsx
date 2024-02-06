056c76d54e769c3c0e444d91cd758923003527f3
Daniel RG <21danielrondong@gmail.com>
2024-02-06 07:37:26 +0100

    manage posts states

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,17 +5,31 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-
-      <ul className={classes.posts}>
-        <Post author={"Arturo"} body={"Project"} />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some posts!</p>
+        </div>
+      )}
     </>
   );
 }
